4ad376c99b4d4ed32b8a0d72fc67e18f579d18c9
pt888 <49671598+pt888@users.noreply.github.com>
2022-01-26 21:13:12 +0530

    Time: 213 ms (48.21%), Space: 0B (100.00%) - LeetHub

--- 176-second-highest-salary/176-second-highest-salary.sql
+++ 176-second-highest-salary/176-second-highest-salary.sql
@@ -1,2 +1,12 @@
 # Write your MySQL query statement below
-select max(salary) as SecondHighestSalary from employee where salary not in (select max(salary) from employee)
+# select max(salary) as SecondHighestSalary from employee where salary not in (select max(salary) from employee)
+# select salary from employee where salary in 
+
+
+# Select ifnull(
+#     (SELECT distinct salary as SecondHighestSalary from Employee order by salary desc limit 1 offset 1)
+#     ,null)
+#     as SecondHighestSalary;
+    
+    
+Select ifnull((SELECT distinct salary as SecondHighestSalary from Employee order by salary desc limit 1 offset 1),null) as SecondHighestSalary;
