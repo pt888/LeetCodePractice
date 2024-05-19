5db54cf69d2c59c7d8afedb5ae47a982d0fec662
pt888 <49671598+pt888@users.noreply.github.com>
2024-05-19 15:56:55 +0530

    Time: 0 ms, Memory: 7.4 MB - LeetHub

---
+++ reverse-integer/reverse-integer.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int ans=0;
+        while(x != 0){
+            int t = x % 10;
+            x = x/10;
+            if(ans > INT_MAX / 10 || (ans == INT_MAX / 10 && t > 7)) return 0;
+            if(ans < INT_MIN / 10 || (ans == INT_MIN / 10 && t < -8)) return 0;
+            ans= ans * 10 + t;
+        }
+        return ans;
+    }
+};
